43cf3fb642927550003e545744da584bcda59813
Abhay <leader.abhay002@gmail.com>
2022-05-30 22:36:09 +0530

    Home Page Initiated and Grid-Gallery Implemented

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,22 @@
+import Image from 'next/image'
 import React from 'react'
 
 export default function Header() {
   return (
-    <div>
-        
+    <div className="sticky top-0 flex p-2 px-2 bg-amber-500 shadow-lg rounded-sm items-center">
+      {/* LEFT */}
+      <div className="flex-1 font-extralight text-2xl">
+        {/* <Image src="/Images/mehndi-logo.png" layout="fill" /> */}
+        <p>Sisters Mehndi</p>
+      </div>
+      {/* RIGHT */}
+      <div className="flex space-x-8 mr-4">
+        <p className="cursor-pointer hover:text-lg">Home</p>
+        <p className="cursor-pointer hover:text-lg">Services</p>
+        <p className="cursor-pointer hover:text-lg">Gallery</p>
+        <p className="cursor-pointer hover:text-lg">About</p>
+        <p className="cursor-pointer hover:text-lg">Contact</p>
+      </div>
     </div>
   )
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Header from '../components/Header'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Header />
+      <Component {...pageProps} />
+    </div>
+  )
 }
 
 export default MyApp
