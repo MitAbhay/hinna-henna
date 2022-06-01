a42d789d86e7461a557267a7dc7fc17918856802
Abhay <leader.abhay002@gmail.com>
2022-06-01 16:01:44 +0530

    Gallery Grid Completed

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 
 export default function Header() {
   return (
-    <div className="sticky top-0 flex p-2 px-2 bg-amber-500 shadow-lg rounded-sm items-center">
+    <div className="sticky top-0 flex p-2 px-2 bg-amber-500 shadow-lg rounded-sm items-center z-50">
       {/* LEFT */}
       <div className="flex-1 font-extralight text-2xl">
         {/* <Image src="/Images/mehndi-logo.png" layout="fill" /> */}
--- pages/gallery.tsx
+++ pages/gallery.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
-import Gallery from 'react-grid-gallery'
-import { IMAGES } from '../public/Gallery-Images/Images'
+import Gallery from 'react-photo-gallery'
+// import Gallery from 'react-grid-gallery'
+import { photos } from '../public/Gallery-Images/Images'
 
 export default function gallery() {
 
   return (
-    <div className="mt-16">
-      <Gallery images={IMAGES} />,
+    <div className="mt-16 mx-10 items-center text-center">
+      <Gallery direction='column' photos={photos} />,
     </div>
   )
 }
