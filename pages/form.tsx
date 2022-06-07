2e8bac9d8ae57afd56f7ae1a950df050506d1002
Abhay <leader.abhay002@gmail.com>
2022-06-07 21:05:25 +0530

    react-hook-form added

--- pages/form.tsx
+++ pages/form.tsx
@@ -9,8 +9,25 @@ function form() {
           <input type="text" name="name" placeholder="Enter your name" />
         </div>
         <div>
-            <p>Email</p>
-            <input type="email" name="email" placeholder="Enter your email" />
+          <p>Email</p>
+          <input type="email" name="email" placeholder="Enter your email" />
+        </div>
+        <div>
+          <p>Date</p>
+          <input type="date" name="date" placeholder="Select Date" />
+        </div>
+        <div>
+          <p>Time</p>
+          <input type="time" name="time" placeholder="Select Time" />
+        </div>
+        <div>
+          <p>Event</p>
+          <select name="event">
+            <option value="volvo">Bridal Mehndi</option>
+            <option value="saab">Saab</option>
+            <option value="opel">Opel</option>
+            <option value="audi">Audi</option>
+          </select>
         </div>
       </form>
     </div>
