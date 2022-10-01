a94ce9f83f62a54c69adfe4d83a867c6ca2b40a2
rezwan2525 <rezwan2525@gmail.com>
2022-10-01 15:59:03 +0600

    Added overriding css to increase padding for carousel control arrow

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import '../styles/globals.css'
+import '../styles/custom-styles.css'
 import type { AppProps } from 'next/app'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
