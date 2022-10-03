62a6efcff00ea12a99993281bb214019cf8d8819
Saurav-Singh-Rauthan <sauravsinghrauthan@gmail.com>
2022-10-03 23:02:39 +0530

    navbar desktop resolves #26

--- components/Navbar/NavbarDesk/NavbarDesk.js
+++ components/Navbar/NavbarDesk/NavbarDesk.js
@@ -1,82 +1,97 @@
 import React from 'react'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 import Styles from './NavbarDesk.module.css'
 import { FaUserAlt, FaQuestion } from 'react-icons/fa'
 
 const navbarDesk = (props) => {
-  let links = (
-    <React.Fragment>
-      <Link
-        className={({ isActive }) =>
-          isActive ? [Styles.active, Styles.link].join(' ') : Styles.link
-        }
-        href="/account"
-      >
-        <FaUserAlt />
-      </Link>
-      <Link
-        className={({ isActive }) =>
-          isActive ? [Styles.active, Styles.link].join(' ') : Styles.link
-        }
-        href="/help"
-      >
-        <FaQuestion />
-      </Link>
-    </React.Fragment>
-  )
+  const router = useRouter()
 
-  if (!props.isAuthenticated) {
-    links = (
-      <Link
-        className={({ isActive }) =>
-          isActive ? [Styles.active, Styles.link].join(' ') : Styles.link
-        }
-        href="/auth"
-      >
-        Login / Register
-      </Link>
-    )
-  }
+  //  uncomment after implementing auth
+  // let links = (
+  //   <React.Fragment>
+  //     <Link
+  //       className={
+  //         router.pathname === '/' ? [Styles.active, Styles.link].join(' ') : Styles.link
+  //       }
+  //       href="/account"
+  //     >
+  //       <FaUserAlt />
+  //     </Link>
+  //     <Link
+  //       className={
+  //         router.pathname === '/' ? [Styles.active, Styles.link].join(' ') : Styles.link
+  //       }
+  //       href="/help"
+  //     >
+  //       <FaQuestion />
+  //     </Link>
+  //   </React.Fragment>
+  // )
 
   return (
     <React.Fragment>
       <div className={Styles.links}>
-        <Link
-          className={({ isActive }) =>
-            isActive ? [Styles.active, Styles.link].join(' ') : Styles.link
-          }
-          href="/"
-        >
-          HOME
+        <Link href="/">
+          <a
+            className={
+              router.pathname === '/'
+                ? [Styles.active, Styles.link].join(' ')
+                : Styles.link
+            }
+          >
+            HOME
+          </a>
         </Link>
-        <Link
-          className={({ isActive }) =>
-            isActive ? [Styles.active, Styles.link].join(' ') : Styles.link
-          }
-          href="/dashboard"
-        >
-          DASHBOARD
+        <Link href="/services">
+          <a
+            className={
+              router.pathname === '/services'
+                ? [Styles.active, Styles.link].join(' ')
+                : Styles.link
+            }
+          >
+            SERVICES
+          </a>
         </Link>
-        <Link
-          className={({ isActive }) =>
-            isActive ? [Styles.active, Styles.link].join(' ') : Styles.link
-          }
-          href="/add-record"
-        >
-          ADD RECORD
+        <Link href="/gallery">
+          <a
+            className={
+              router.pathname === '/gallery'
+                ? [Styles.active, Styles.link].join(' ')
+                : Styles.link
+            }
+          >
+            GALLERY
+          </a>
         </Link>
-        <Link
-          className={({ isActive }) =>
-            isActive ? [Styles.active, Styles.link].join(' ') : Styles.link
-          }
-          href="/fuel-prices"
-        >
-          FUEL PRICES
+        <Link href="/about">
+          <a
+            className={
+              router.pathname === '/about'
+                ? [Styles.active, Styles.link].join(' ')
+                : Styles.link
+            }
+          >
+            ABOUT
+          </a>
+        </Link>
+        <Link href="/contact">
+          <a
+            className={
+              router.pathname === '/contact'
+                ? [Styles.active, Styles.link].join(' ')
+                : Styles.link
+            }
+          >
+            CONTACT
+          </a>
         </Link>
       </div>
 
-      <div className={Styles.links}>{links}</div>
+      {/* uncomment after implementing auth */}
+      {/* <div className={Styles.links}>{links}</div>  */}
     </React.Fragment>
   )
 }
