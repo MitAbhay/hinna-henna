fe2461f5adae1e3d17a55f78788851714a560d09
aaheli8 <sadhukhanaheli@gmail.com>
2022-10-05 23:25:09 +0530

    Gallery page enhancement

--- pages/gallery.tsx
+++ pages/gallery.tsx
@@ -1,13 +1,43 @@
-import React from 'react'
-import Gallery from 'react-photo-gallery'
-// import Gallery from 'react-grid-gallery'
-import { photos } from '../public/Gallery-Images/Images'
+import React, { useState, useCallback } from "react";
+import { render } from "react-dom";
+import Gallery from "react-photo-gallery";
+import Carousel, { Modal, ModalGateway } from "react-images";
+import { photos } from '../public/Gallery-Images/Images';
 
-export default function gallery() {
+export default function gallery(){
+  const [currentImage, setCurrentImage] = useState(0);
+  const [viewerIsOpen, setViewerIsOpen] = useState(false);
+
+  const openLightbox = useCallback((event, { photo, index }) => {
+    setCurrentImage(index);
+    setViewerIsOpen(true);
+  }, []);
+
+  const closeLightbox = () => {
+    setCurrentImage(0);
+    setViewerIsOpen(false);
+  };
 
   return (
+    
     <div className="mt-16 mx-10 items-center text-center">
-      <Gallery direction='column' photos={photos} />,
+      <Gallery photos={photos} direction={"column"} onClick={openLightbox} />
+      <ModalGateway>
+        {viewerIsOpen ? (
+          <Modal onClose={closeLightbox}>
+            <Carousel
+              currentIndex={currentImage}
+              views={photos.map(x => ({
+                ...x,
+                srcset: x.srcSet,
+                caption: x.title,
+                width:7
+              }))}
+            />
+          </Modal>
+        ) : null}
+      </ModalGateway>
     </div>
-  )
+  );
 }
+
