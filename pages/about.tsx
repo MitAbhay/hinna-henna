29e4c9a25c86b2472bc265e5867122e3b6ecaa0e
Aman Anand <79956798+amananand917@users.noreply.github.com>
2022-10-14 18:11:15 +0530

    Update about.tsx

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,7 +1,77 @@
+import Image from 'next/image'
 import React from 'react'
 
-export default function about() {
+
+
+
+const About = () => {
   return (
-    <div>This is hinna henna about pagei</div>
+<div className="containerMain">
+  <div className="containerAbout">
+    <div className="about-content">
+      <h2 className="headerAbout">About ME</h2>
+      <p>
+        This is platform for mehndi, can do Mehndi/Henna for just about any occasion. We Specialize in Bridal Mehndi, Sangeet Parties, Engagements, Baby Showers, Birthday Parties, Ladies Night outs, School Events, Corporate Events and Fundraisers. Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis reprehenderit quas nesciunt odio quia distinctio nostrum, amet error veniam nobis. Dolor architecto id molestiae aliquid laudantium iusto molestias commodi cum.
+      </p>
+    </div>
+    <div className="container-image">
+      <img src="https://www.beyoung.in/blog/wp-content/uploads/2020/01/peacock-compressed-931x1024.jpg" />
+    </div>
+  </div>
+  <div className="section-margin-top about-section">
+    <div className="row">
+      <div className="tm-section-header">
+        <div className="col-lg-3 col-md-3 col-sm-3">
+          <hr/>
+        </div>
+        <div className="col-lg-6 col-md-6 col-sm-6">
+          <h2 className="tm-section-title">WHO WE ARE</h2>
+        </div>
+        <div className="col-lg-3 col-md-3 col-sm-3">
+          <hr/>
+        </div>
+      </div>
+        </div>
+        
+
+
+    <div className="row">
+      <div className="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+        <div className="tm-about-box-1">
+              <a href="#"><img src="https://www.mintface.xyz/content/images/2021/08/QmTndiF423kjdXsNzsip1QQkBQqDuzDhJnGuJAXtv4XXiZ-1.png" alt="img" className="tm-about-box-1-img"/></a>
+          <h3 className="tm-about-box-1-title">Abc <span>(Founder)</span></h3>
+          <p className="margin-bottom-15 gray-text">Proin gravida nibhvell aliquet. Aenean sollicitudin bibum auctor nisi elit.</p>
+          
+        </div>
+      </div>
+      <div className="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+        <div className="tm-about-box-1">
+              <a href="#"><img src="https://www.mintface.xyz/content/images/2021/08/QmTndiF423kjdXsNzsip1QQkBQqDuzDhJnGuJAXtv4XXiZ-1.png" alt="img" className="tm-about-box-1-img"/></a>
+          <h3 className="tm-about-box-1-title">Abc <span>( Co-Founder )</span></h3>
+          <p className="margin-bottom-15 gray-text">Proin gravida nibhvell aliquet. Aenean sollicitudin bibum auctor nisi elit.</p>
+
+        </div>
+      </div>
+      <div className="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+        <div className="tm-about-box-1">
+              <a href="#"><img src="https://www.mintface.xyz/content/images/2021/08/QmTndiF423kjdXsNzsip1QQkBQqDuzDhJnGuJAXtv4XXiZ-1.png" alt="img" className="tm-about-box-1-img"/></a>
+          <h3 className="tm-about-box-1-title">AB <span>( General Manager )</span></h3>
+          <p className="margin-bottom-15 gray-text">Proin gravida nibhvell aliquet. Aenean sollicitudin bibum auctor nisi elit.</p>
+          
+        </div>
+      </div>
+      <div className="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+        <div className="tm-about-box-1">
+              <a href="#"><img src="https://www.mintface.xyz/content/images/2021/08/QmTndiF423kjdXsNzsip1QQkBQqDuzDhJnGuJAXtv4XXiZ-1.png" alt="img" className="tm-about-box-1-img"/></a>
+          <h3 className="tm-about-box-1-title">ABC <span>( Manager )</span></h3>
+          <p className="margin-bottom-15 gray-text">Proin gravida nibhvell aliquet. Aenean sollicitudin bibum auctor nisi elit.</p>
+          
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
   )
 }
+
+export default About
